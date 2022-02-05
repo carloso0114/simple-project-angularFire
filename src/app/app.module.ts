65b9729e425c134aaf640282669e6f5b1b8c98f6
carloso0114 <carlossospinag@gmail.com>
2022-02-05 16:19:43 -0500

    initial implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'prueba3-7d77a',
+    appId: '1:21532858218:web:7ede5d36e9f7a00521b713',
+    storageBucket: 'prueba3-7d77a.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyBKl8o2EzpR9a9ztIZgoWfRe2MpziBLEdw',
+    authDomain: 'prueba3-7d77a.firebaseapp.com',
+    messagingSenderId: '21532858218',
+  },
   production: true
 };
