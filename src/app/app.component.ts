3ce00849deeff7047197f035afe449d07da7cec2
carloso0114 <carlossospinag@gmail.com>
2022-02-07 13:21:00 -0500

    implement basic routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from './api.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ApiService]
 })
 export class AppComponent {
-  constructor(private db: ApiService) {
+  constructor() {
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+// importing forms module
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
+// Components
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { ShowInfoComponent } from './show-info/show-info.component';
+import { DataFormComponent } from './data-form/data-form.component';
+
+//AngularFire
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { ShowInfoComponent } from './show-info/show-info.component';
-import { DataFormComponent } from './data-form/data-form.component';
+
+// importing Routes module
+import { Routes, RouterModule } from '@angular/router';
+
+// Routing
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent},
+]; 
 
 @NgModule({
   declarations: [
     AppComponent,
     ShowInfoComponent,
-    DataFormComponent
+    DataFormComponent,
+    UsersComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +43,8 @@ import { DataFormComponent } from './data-form/data-form.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
